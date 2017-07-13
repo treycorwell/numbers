8748915004815d23027e2a10dec0e3100fc94512
vcmccormick <vcmccormick@gmail.com>
2017-07-13 14:09:36 -0400

    div by 3 5 and 15 success

--- minedminds_array.rb
+++ minedminds_array.rb
@@ -1,2 +1,18 @@
 def create_mined_minds_array()
+	mm_array = []
+	counter = 1
+	100.times do
+		
+	if counter % 15 == 0
+		mm_array.push("minedminds")
+	elsif counter % 5 == 0
+		mm_array.push("minds")
+	elsif counter % 3 == 0
+		mm_array.push("mined")
+	else
+		mm_array.push(counter)
+	end
+	counter = counter + 1
+end
+	mm_array
 end
--- minedminds_array_test.rb
+++ minedminds_array_test.rb
@@ -11,4 +11,31 @@ class TestMindedMinds_Array < Minitest::Test
 		mm_array = create_mined_minds_array()
 		assert_equal(Array, mm_array.class)
 	end
+
+	def test_array_length_is_100
+		mm_array = create_mined_minds_array()
+		assert_equal(100, mm_array.length)
+	end
+
+	def test_zero_position_in_array_is_one
+		mm_array = create_mined_minds_array()
+		assert_equal(1, mm_array[0])
+	end
+
+	def test_second_position_in_array_is_mined
+		mm_array = create_mined_minds_array()
+		assert_equal("mined", mm_array[2])
+	end
+
+	def test_fifth_position_in_array_is_mined
+		mm_array = create_mined_minds_array()
+		assert_equal("minds", mm_array[4])
+	end
+
+	def test_fifteen_position_in_array_is_minedminds
+		mm_array = create_mined_minds_array()
+		assert_equal("minedminds", mm_array[14])
+	end
 end
+
+
